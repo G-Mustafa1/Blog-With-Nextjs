3b48e070cd6019fc0201ecb9ecb106e63e76ebb3
Ghulam Mustafa <gmustufa1255@gmail.com>
2025-08-20 01:58:12 +0500

    Canhged Layout Tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-gray-100">
+      <body className={`bg-gray-100 ${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         <main className="p-6 max-w-7xl mx-auto">{children}</main>
         <Footer />
